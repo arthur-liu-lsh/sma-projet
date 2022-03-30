a844fe8f4e923a85d721798fdde88d07832f7cec
arthurliu2022 <63053605+arthurliu2022@users.noreply.github.com>
2022-03-31 00:29:24 +0200

    final commit

--- benchmark.py
+++ benchmark.py
@@ -5,14 +5,14 @@ def main():
     
     n_iter = 1000
 
-    # sum = 0
-    # for i in range(n_iter):
-    #    world_EpsGreedy = World(10, 0.2,3, "EpsGreedy", False, 0.1)
-    #    while not world_EpsGreedy.check_validity(5, 0.7):
-    #         world_EpsGreedy = World(10, 0.2,3, "EpsGreedy", False, 0.1)
-    #         sum += world_EpsGreedy.simulate()
+    sum = 0
+    for i in range(n_iter):
+       world_EpsGreedy = World(10, 0.2,3, "EpsGreedy", False, 0.1)
+       while not world_EpsGreedy.check_validity(5, 0.7):
+            world_EpsGreedy = World(10, 0.2,3, "EpsGreedy", False, 0.1)
+            sum += world_EpsGreedy.simulate()
     
-    # print("EpsGreedy 10x10, 20", "%", "d'obstacles", n_iter, "itérations :",sum/n_iter)
+    print("EpsGreedy 10x10, 20", "%", "d'obstacles", n_iter, "itérations :",sum/n_iter)
 
     sum = 0
     for i in range(n_iter):
@@ -21,7 +21,7 @@ def main():
             world_AStar = World(10, 0.2,3, "AStar", False, 0.1)
         sum += world_AStar.simulate()
     
-    print("A*",sum/n_iter)
+    print("A* 10x10, 20", "%", "d'obstacles", n_iter, "itérations :",sum/n_iter)
 
 if __name__=="__main__":
    main()
